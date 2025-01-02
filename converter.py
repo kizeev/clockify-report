f3ba0c4eb98746e57c928864845825887c3a520c
kizeev <kizalvic@gmail.com>
2025-01-02 18:33:32 +0300

    edit converter due to converting service changed

--- converter.py
+++ converter.py
@@ -1,15 +1,14 @@
+import re
 import requests
 
 from datetime import date
-from typing import Dict, List, Union
+from io import StringIO
+from lxml import etree
+from time import sleep
 
 
-API_URL = 'https://exchangerates.org/ajax/getChart'
-HEADERS = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'X-Requested-With': 'XMLHttpRequest',
-}
-BODY = 'base_id=49&tocode=USD&days=30&amount={}'
+CONVERTER_URL = 'https://exchangerates.org/{}-eur-to-usd'
+RETRY_TIMES = 10
 
 
 def convert_to_usd(euro_price: str, last_day: date) -> str:
@@ -18,25 +17,31 @@ def convert_to_usd(euro_price: str, last_day: date) -> str:
         last_day = today
     try:
         euro_price = euro_price.replace('.', '').replace(',', '.')
-        api_data = _get_data_from_api(euro_price)
-        usd_price = _parse_api_data(api_data, last_day)
+        response = _get_response_with_converted_price(euro_price)
+        usd_price = _parse_response(response, last_day)
         return f'{usd_price:.2f}'
     except Exception as e:
         raise RuntimeError(f'Failed to convert {euro_price} EUR to USD: {e}')
 
 
-def _get_data_from_api(euro_price: str) -> Dict[str, List[Union[str, float]]]:
-    try:
-        response = requests.post(
-            API_URL, BODY.format(euro_price), headers=HEADERS)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        raise RuntimeError(f'Failed to retrieve data from exchange API: {e}')
+def _get_response_with_converted_price(
+        euro_price: str) -> str:
+    for retry_time in reversed(range(RETRY_TIMES)):
+        response = requests.get(CONVERTER_URL.format(euro_price))
+        if response.status_code == 200:
+            return response.text
+        print(f'Failed to convert, retry time {retry_time}')
+        sleep(3)
+    else:
+        raise RuntimeError('Failed to retrieve data from exchange site')
 
 
-def _parse_api_data(
-        api_data: Dict[str, List[Union[str, float]]], last_day: date) -> float:
-    struct_data = dict(zip(api_data['dates'], api_data['rates']))
-    usd_price = struct_data[last_day.strftime("%Y-%m-%d")]
+def _parse_response(response: str, last_day: date) -> float:
+    formatted_last_day = last_day.strftime("%d/%m/%Y")
+    parser = etree.HTMLParser(recover=True)
+    html = etree.parse(StringIO(response), parser)
+    [raw_price] = html.xpath(
+        f'//td[contains(., "{formatted_last_day}")]'
+        '/following-sibling::td/text()')
+    usd_price = re.findall(r'\b(\d+\.\d+)\sUSD', raw_price)[0]
     return float(usd_price)
